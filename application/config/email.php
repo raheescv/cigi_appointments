73a692ccd5aaf84cb1ade46efa1791f187a97f69
raheescv <raheescv1992@gmail.com>
2023-05-22 20:55:17 +0530

    Update email.php

--- application/config/email.php
+++ application/config/email.php
@@ -7,10 +7,10 @@
 $config['useragent'] = 'Easy!Appointments';
 $config['protocol'] = 'mail'; // or 'smtp'
 $config['mailtype'] = 'html'; // or 'text'
-// $config['smtp_debug'] = '0'; // or '1'
-// $config['smtp_auth'] = TRUE; //or FALSE for anonymous relay.
-// $config['smtp_host'] = '';
-// $config['smtp_user'] = '';
-// $config['smtp_pass'] = '';
-// $config['smtp_crypto'] = 'ssl'; // or 'tls'
-// $config['smtp_port'] = 25;
+$config['smtp_debug'] = '0'; // or '1'
+$config['smtp_auth'] = TRUE; //or FALSE for anonymous relay.
+$config['smtp_host'] = 'smtp.googlemail.com';
+$config['smtp_user'] = 'booking@cigi.org';
+$config['smtp_pass'] = 'booking@1234';
+$config['smtp_crypto'] = 'ssl'; // or 'tls'
+$config['smtp_port'] = 465;
