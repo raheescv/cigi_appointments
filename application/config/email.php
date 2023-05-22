9000aa6c7394a2da703c3abb8b6d3e55fd570132
raheescv <raheescv1992@gmail.com>
2023-05-22 21:04:53 +0530

    Update email.php

--- application/config/email.php
+++ application/config/email.php
@@ -5,11 +5,11 @@
 // @link https://codeigniter.com/user_guide/libraries/email.html
 
 $config['useragent'] = 'Easy!Appointments';
-$config['protocol'] = 'mail'; // or 'smtp'
+$config['protocol'] = 'smtp'; // or 'smtp'
 $config['mailtype'] = 'html'; // or 'text'
 $config['smtp_debug'] = '0'; // or '1'
 $config['smtp_auth'] = TRUE; //or FALSE for anonymous relay.
-$config['smtp_host'] = 'smtp.googlemail.com';
+$config['smtp_host'] = 'smtp.gmail.com';
 $config['smtp_user'] = 'booking@cigi.org';
 $config['smtp_pass'] = 'booking@1234';
 $config['smtp_crypto'] = 'ssl'; // or 'tls'
